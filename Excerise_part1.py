da6887cb1325e9b1a2425f55083a0268150eb6e2
Yli <salazarylizanicole@gmail.com>
2023-01-14 14:21:18 +0800

    adding min method

---
+++ Excerise_part1.py
@@ -0,0 +1,42 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+    def find_min(self):
+        if self.left:
+            return self.left.find_min()
+        else:
+            return self.data
+
+
+def build_tree(elements):
+    print("Elements:", elements)
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    Name = ["Y","L","I", "Z","A", "N", "I", "C", "O", "L", "E", "R", "E", "Q", "U", "I", "L", "L", "A", "S", "S", "A","L", "A", "Z", "A", "R"]
+    name_tree = build_tree(Name)
+
+
+print("Found Min : ", name_tree.find_min())
